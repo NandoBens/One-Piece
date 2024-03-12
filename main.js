54fea8424dfb9949115033d45959d4b48d62bde4
NandoBens <162718602+NandoBens@users.noreply.github.com>
2024-03-12 11:24:00 -0300

    Add files via upload

---
+++ main.js
@@ -0,0 +1,10 @@
+
+function mudarimagemfora()
+{
+    document.getElementById("imagem").src ="https://p2.trrsf.com/image/fget/cf/774/0/images.terra.com/2021/11/04/one-piece-1000.png";
+}
+
+function mudarimagemdentro()
+{
+    document.getElementById("imagem").src ="https://wallpapercrafter.com/desktop/102375-One-Piece-Monkey-D.-Luffy-Sabo-Portgas-D.-Ace.jpg";
+}
